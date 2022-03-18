4820fdbca14dd6c546c2a32252de0f43d41e1f6d
Israel Oliveira <israel.oliveira.933@gmail.com>
2022-03-18 10:05:32 -0300

    final project

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//Module
+import { SharedModule } from './../shared/shared.module';
+import { RoutingModule } from './routing.module';
+
+//Pages
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    DetailsComponent,
+
+  ],
+  imports: [
+    CommonModule,
+    RoutingModule,
+    SharedModule,
+    
+    
+  ]
+})
+export class PagesModule { }
